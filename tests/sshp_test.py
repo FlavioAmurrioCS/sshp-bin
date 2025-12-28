2e5ef901f6c320d49c75872ad8a43cee5ca709c3
Flavio Amurrio <25621374+FlavioAmurrioCS@users.noreply.github.com>
2025-12-27 23:29:49 -0500

    Init

---
+++ tests/sshp_test.py
@@ -0,0 +1,20 @@
+# flake8: noqa: S603
+from __future__ import annotations
+
+import logging
+import subprocess
+import sys
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+
+
+def test_help() -> None:
+    sshp_bin = Path(sys.executable).parent / "sshp"
+    assert sshp_bin.exists(), f"sshp binary not found at {sshp_bin}"
+    result = subprocess.run([sshp_bin, "--help"], check=False, capture_output=True, text=True)
+    if result.returncode != 0:
+        logger.error(result.stderr)
+        msg = f"Error running {sshp_bin} --help"
+        raise AssertionError(msg)
